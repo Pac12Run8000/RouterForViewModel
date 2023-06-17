803009884a78183181e5107b96fcd5eb2bd03fc4
Pac12Run8000 <gangstarr78992@>
2023-06-17 13:41:37 -0700

    Initial Commit

---
+++ RouterForViewModel/RouterForViewModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterForViewModelApp.swift
+//  RouterForViewModel
+//
+//  Created by Michelle Grover on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct RouterForViewModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
