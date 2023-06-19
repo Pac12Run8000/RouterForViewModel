cdbcc2afe5262b1185524fca63712dbda05f43d6
Pac12Run8000 <gangstarr78992@>
2023-06-18 18:36:50 -0700

    Everything is working

--- RouterForViewModel/Views/ContentView.swift
+++ RouterForViewModel/Views/ContentView.swift
@@ -9,25 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var navigateToDetailScreen = false
+    @State private var navigateToDashboardScreen = false
     var body: some View {
         NavigationView {
             VStack {
-                
                 NavigationLink(destination: UpdateProfileView(), isActive: $navigateToDetailScreen) {
                                     EmptyView()
-                                }
+                }
+                NavigationLink(destination: DashboardView(), isActive: $navigateToDashboardScreen) {
+                                    EmptyView()
+                }
+                Button {
+                    self.navigateToDashboardScreen = true
+                } label: {
+                    Text("Present Dashboard")
+                }
+
             }
             .navigationTitle("Main Profile")
-            .navigationBarItems(trailing:
-                            Button {
-                                navigateToDetailScreen = true
-                            } label: {
-                                Text("Update profile")
-                            }
-                        )
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(trailing: UpdateProfileButton(navigateToDetailScreen: $navigateToDetailScreen))
+            
         }
     }
 }
+
+struct PresentDashBoard: View {
+    @Binding var navigateToDashboardScreen: Bool
+
+    var body: some View {
+        Button {
+            navigateToDashboardScreen = true
+        } label: {
+            Text("Present DashBoard")
+        }
+    }
+}
+
+struct UpdateProfileButton: View {
+    @Binding var navigateToDetailScreen: Bool
+
+    var body: some View {
+        Button {
+            navigateToDetailScreen = true
+        } label: {
+            Text("Update profile")
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
